93dab8bb97a068f7789961d404e66b1cb656d515
DrOtter3000 <info@darknet-online.com>
2025-09-25 11:44:37 +0200

    lerp boost function

--- player/player.gd
+++ player/player.gd
@@ -4,7 +4,9 @@ extends Node3D
 
 @export var max_hitpoints := 5
 @export var speed := 10.0
-@export var boost_multiplier := 10.0
+@export var max_boost := 10.0
+@export var boost_accel := 2.0
+@export var boost_decel := 1.0
 @export var max_pos_x := 10.0
 @export var max_pos_y := 10.0
 
@@ -15,9 +17,9 @@ var hitpoints: int = max_hitpoints
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		main_animation.speed_scale = boost_multiplier
+		main_animation.speed_scale = lerp(main_animation.speed_scale, max_boost, delta * boost_accel)
 	else:
-		main_animation.speed_scale = 1
+		main_animation.speed_scale = lerp(main_animation.speed_scale, 1.0, delta * boost_decel)
 
 func _ready() -> void:
 	model.max_pos_x = max_pos_x
